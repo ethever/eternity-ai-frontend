5e53ec419e88edbf38c20726176a0b2cbacb5716
famouscat <onismaa@gmail.com>
2023-04-29 15:57:56 +0800

    chore: add framer motion

--- apps/main/src/App.tsx
+++ apps/main/src/App.tsx
@@ -1,17 +1,28 @@
-import { useMemo } from "react";
+import { ReactNode, useMemo } from "react";
 import ResponsiveAppBar from "./components/appbar";
 import { tabsIndexAtom } from "./state";
 import { Index } from "./views";
 import { useAtom } from "jotai";
 import { CreateProfile } from "./components/createProfile";
 import { Container, Typography } from "@mui/joy";
+import { motion, AnimatePresence } from "framer-motion";
 
 function App() {
   const [tabsIndex] = useAtom(tabsIndexAtom);
 
   const view = useMemo(() => {
-    if (tabsIndex === 3) return <Index />;
-    if (tabsIndex === 1) return <CreateProfile />;
+    if (tabsIndex === 3)
+      return (
+        <MotionWraper key="3">
+          <Index />
+        </MotionWraper>
+      );
+    if (tabsIndex === 1)
+      return (
+        <MotionWraper key="1">
+          <CreateProfile />
+        </MotionWraper>
+      );
     return (
       <Container
         sx={{
@@ -27,8 +38,21 @@ function App() {
   return (
     <>
       <ResponsiveAppBar />
-      {view}
+      <AnimatePresence>{view}</AnimatePresence>
     </>
+  );
+}
+
+function MotionWraper({ key, children }: { key: string; children: ReactNode }) {
+  return (
+    <motion.div
+      key={key}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+    >
+      {children}
+    </motion.div>
   );
 }
 
