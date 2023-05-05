fb5df570127dc24537a6db1aa0025b5ce7b2053f
famouscat <onismaa@gmail.com>
2023-05-06 01:54:41 +0800

    using wrangler to deploy publish

--- apps/web_v2/src/hooks/useNavigateToDAPP.ts
+++ apps/web_v2/src/hooks/useNavigateToDAPP.ts
@@ -1,5 +1,5 @@
 export default function useNavigateToDAPP() {
   const mode = import.meta.env.MODE;
   if (mode === "development") return "http://localhost:8000";
-  return "https://eternity-ai-frontend.pages.dev";
+  return "https://eternity-ai-dapp-frontend.pages.dev/";
 }
